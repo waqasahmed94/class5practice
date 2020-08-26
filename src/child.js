e370a6c7ecc90ccc62845f16a4cc3b25e28b6d43
waqasahmed94 <66775969+waqasahmed94@users.noreply.github.com>
2020-08-26 07:19:35 +0000

    update child.js

--- src/child.js
+++ src/child.js
@@ -7,7 +7,8 @@ const Child = () => {
     let countervalue = useContext(CounterContext)
     return (
         <div>
-            <h1>THis Increment Done with Counter Context</h1>
+            <h1>This Increment Done with Counter Context API</h1>
+            <h1>This Is first child</h1>
 
     <h2>Counter Value is:-{countervalue[0]}</h2>
     <button onClick={() =>{countervalue[1](++countervalue[0])}}>Context Increment</button>
