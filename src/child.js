c45dec6e75780fcfe67834ba96c5b4ca5083c150
waqasahmed94 <66775969+waqasahmed94@users.noreply.github.com>
2020-08-26 10:37:00 +0000

    update child.js

--- src/child.js
+++ src/child.js
@@ -8,7 +8,7 @@ const Child = () => {
     return (
         <div>
             <h1>This Increment Done with Counter Context API</h1>
-            <h1>This Is first child.</h1>
+            <h1>This Is first child.......</h1>
 
     <h2>Counter Value is:-{countervalue[0]}</h2>
     <button onClick={() =>{countervalue[1](++countervalue[0])}}>Context Increment</button>
